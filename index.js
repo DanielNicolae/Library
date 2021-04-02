6aa2b83ca7dcccb304e97118aaafa54627d4722f
DanielNicolae <deadlypoet@protonmail.com>
2021-04-02 18:43:21 +0300

    Added the insertBook function in index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+  title = "hello";
+  author = "hi";
+  let book = new Book(title, author);
   myLibrary.push(book);
 }
 
-addBookToLibrary.prototype = Object.create(Book.prototype);
+function insertBook(title, author) {
+  const table = document.getElementById("library");
+  const line = document.createElement("tr");
+  const titleCell = document.createElement("td");
+  const authorCell = document.createElement("td");
+  titleCell.innerText = title;
+  authorCell.innerText = author;
+  line.innerHTML += titleCell;
+  line.innerHTML += authorCell;
+}
